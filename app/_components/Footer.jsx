2c90343344a71d22d1353f4895d96773d704ba88
Seyyedi <mr.seyyedi76@gmail.com>
2024-08-22 18:20:46 +0330

    Create footer section

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 
 const Footer = () => {
@@ -6,11 +7,36 @@ const Footer = () => {
       <MaxWidthWrapper>
         <div className="border-t border-gray-200" />
 
-        <diva className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center">
+        <div className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center">
           <div className="text-center md:text-left pb-2 md:pb-0">
-            <p className="text-sm text-muted-foreground">&copy;</p>
+            <p className="text-sm text-muted-foreground">
+              &copy;{new Date().getFullYear()} All rights reserved
+            </p>
           </div>
-        </diva>
+
+          <div className="flex items-center justify-center">
+            <div className="flex space-x-8">
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                Terms
+              </Link>
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                Privacy Policy
+              </Link>
+              <Link
+                href="#"
+                className="text-sm text-muted-foreground hover:text-gray-600"
+              >
+                Cookie Policy
+              </Link>
+            </div>
+          </div>
+        </div>
       </MaxWidthWrapper>
     </footer>
   );
